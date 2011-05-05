9fb99b7f96cef90ba01b16a1b6351125ce2ce9ee
punytan <punytan@gmail.com>
2011-05-05 16:07:26 +0900

    initial commit

---
+++ lib/App/ListMiddleware.pm
@@ -0,0 +1,32 @@
+package App::ListMiddleware;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::ListMiddleware -
+
+=head1 SYNOPSIS
+
+  use App::ListMiddleware;
+
+=head1 DESCRIPTION
+
+App::ListMiddleware is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
